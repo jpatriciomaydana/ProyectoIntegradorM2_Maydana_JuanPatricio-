b14f8fabf49df1b3d7376b655778d32c9965bc0c
jpatriciomaydana <maydanajp@gmail.com>
2026-08-09 20:44:27 -0300

    feat: agregar endpoint de health check

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 require('dotenv').config();
 const express = require('express');
-
 const app = express();
-
 app.use(express.json());
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'ok' });
+});
 
 const authorsRouter = require('./routes/authors');
 app.use('/authors', authorsRouter);
 
 const postsRouter = require('./routes/posts');
 app.use('/posts', postsRouter);
+
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./openapi.yaml');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const errorHandler = require('./middlewares/errorHandler');
 app.use(errorHandler);
