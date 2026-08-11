63428e89fecf2b3551432b51d80e01df77947352
jpatriciomaydana <maydanajp@gmail.com>
2026-08-11 14:08:20 -0300

    feat: agregar endpoint de verificacion en la ruta raiz

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+  res.send('¡Servidor funcionando en Railway!');
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
